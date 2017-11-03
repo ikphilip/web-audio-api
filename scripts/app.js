2df8342c4a14041ec0186f81d600575d47e8844b
Philip Curley <philip@interactiveknowledge.com>
2017-11-03 10:24:40 -0400

    init demo with mulitple audio sources and 4 channels

--- scripts/app.js
+++ scripts/app.js
@@ -1,202 +1,44 @@
-// create web audio api context
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioCtx = new AudioContext();
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-// create Oscillator and gain node
-var oscillator = audioCtx.createOscillator();
-var oscillator2 = audioCtx.createOscillator();
+// Collect the distinct audio sources
+var frontLeft = document.querySelector('.front-left');
+var frontRight = document.querySelector('.front-right');
+var surroundLeft = document.querySelector('.surround-left');
+var surroundRight = document.querySelector('.surround-right');
 
-var gainNode = audioCtx.createGain();
-var gainNode2 = audioCtx.createGain();
+// Add controls
+frontLeft.controls = true;
+frontRight.controls = true;
+surroundLeft.controls = true;
+surroundRight.controls = true;
 
+// createMediaElementSource grabs a resource referenced in DOM
+var sourceFL = audioCtx.createMediaElementSource(frontLeft);
+var sourceFR = audioCtx.createMediaElementSource(frontRight);
+var sourceSL = audioCtx.createMediaElementSource(surroundLeft);
+var sourceSR = audioCtx.createMediaElementSource(surroundRight);
 
-// Splitter
-var splitter = audioCtx.createChannelSplitter(2);
-var splitter2 = audioCtx.createChannelSplitter(2);
+// gainNode only has a single I/O so each source must have it's own
+var gainNodeFL = audioCtx.createGain();
+sourceFL.connect(gainNodeFL);
 
-var merger = audioCtx.createChannelMerger(2);
+var gainNodeFR = audioCtx.createGain();
+sourceFR.connect(gainNodeFR);
 
-// connect oscillator to gain node to speakers
+var gainNodeSL = audioCtx.createGain();
+sourceSL.connect(gainNodeSL);
 
-oscillator.connect(gainNode);
-oscillator2.connect(gainNode2);
+var gainNodeSR = audioCtx.createGain();
+sourceSR.connect(gainNodeSR);
 
-gainNode.connect(splitter);
-gainNode2.connect(splitter2);
-// Connect right channel
-splitter.connect(merger, 0, 0)
+// Create an output stream which merges 4 channels
+var merger = audioCtx.createChannelMerger(4);
 
-// Connect left channel
-splitter2.connect(merger, 0, 1);
+// Now merge the streams into individual channels
+gainNodeFL.connect(merger, 0, 0);
+gainNodeFR.connect(merger, 0, 1);
+gainNodeSL.connect(merger, 0, 2);
+gainNodeSR.connect(merger, 0, 3);
 
-// Sent to output
+// Connect to output
 merger.connect(audioCtx.destination);
-
-// create initial theremin frequency and volumn values
-
-var WIDTH = window.innerWidth;
-var HEIGHT = window.innerHeight;
-
-var maxFreq = 6000;
-var maxVol = 0.02;
-
-var initialFreq = 400;
-var initialFreq2 = 400;
-var initialVol = 0.01;
-
-gainNode2.gain.value = initialVol;
-
-// set options for the oscillator
-oscillator.detune.value = 100; // value in cents
-oscillator.frequency.value = initialFreq;
-oscillator.start(0);
-
-oscillator2.detune.value = 100; // value in cents
-oscillator2.frequency.value = initialFreq2;
-oscillator2.start(0);
-
-oscillator.onended = function() {
-  console.log('Your tone has now stopped playing!');
-}
-
-gainNode.gain.value = initialVol;
-
-// Mouse pointer coordinates
-
-var CurX;
-var CurY;
-
-// Get new mouse pointer coordinates when mouse is moved
-// then set new gain and pitch values
-
-document.onmousemove = updatePage;
-
-function updatePage(e) {
-    KeyFlag = false;
-
-    CurX = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
-    CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-    
-    oscillator.frequency.value = (CurX/WIDTH) * maxFreq;
-    gainNode.gain.value = (CurY/HEIGHT) * maxVol;
-
-    canvasDraw();
-}
-
-// mute button
-
-var mute = document.querySelector('.mute');
-
-mute.onclick = function() {
-  if(mute.getAttribute('data-muted') === 'false') {
-    merger.disconnect(audioCtx.destination);
-    mute.setAttribute('data-muted', 'true');
-    mute.innerHTML = "Unmute";
-  } else {
-    merger.connect(audioCtx.destination);
-    mute.setAttribute('data-muted', 'false');
-    mute.innerHTML = "Mute";
-  };
-}
-
-
-
-// canvas visualization
-
-function random(number1,number2) {
-  var randomNo = number1 + (Math.floor(Math.random() * (number2 - number1)) + 1);
-  return randomNo;
-} 
-
-var canvas = document.querySelector('.canvas');
-canvas.width = WIDTH;
-canvas.height = HEIGHT; 
-
-var canvasCtx = canvas.getContext('2d');
-
-function canvasDraw() {
-  if(KeyFlag == true) {
-    rX = KeyX;
-    rY = KeyY;
-  } else {
-    rX = CurX;
-    rY = CurY;
-  }
-  rC = Math.floor((gainNode.gain.value/maxVol)*30);
-  
-  canvasCtx.globalAlpha = 0.2;
-  
-  for(i=1;i<=15;i=i+2) {
-    canvasCtx.beginPath();
-    canvasCtx.fillStyle = 'rgb(' + 100+(i*10) + ',' + Math.floor((gainNode.gain.value/maxVol)*255) + ',' + Math.floor((oscillator.frequency.value/maxFreq)*255) + ')';
-    canvasCtx.arc(rX+random(0,50),rY+random(0,50),rC/2+i,(Math.PI/180)*0,(Math.PI/180)*360,false);
-    canvasCtx.fill();
-    canvasCtx.closePath();     
-  }    
-}
-
-// clear screen
-
-var clear = document.querySelector('.clear');
-
-clear.onclick = function() {
-  canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-// keyboard controls
-
-var body = document.querySelector('body');
-
-var KeyX = 1;
-var KeyY = 0.01;
-var KeyFlag = false;
-
-body.onkeydown = function(e) {
-  KeyFlag = true;
-
-  // 37 is arrow left, 39 is arrow right,
-  // 38 is arrow up, 40 is arrow down
-
-  if(e.keyCode == 37) {
-    KeyX -= 20;
-  };
-
-  if(e.keyCode == 39) {
-    KeyX += 20;
-  };
-
-  if(e.keyCode == 38) {
-    KeyY -= 20;
-  };
-
-  if(e.keyCode == 40) {
-    KeyY += 20;
-  };
-
-  // set max and min constraints for KeyX and KeyY
-
-  if(KeyX < 1) {
-    KeyX = 1;
-  };
-
-  if(KeyX > WIDTH) {
-    KeyX = WIDTH;
-  };
-
-  if(KeyY < 0.01) {
-    KeyY = 0.01;
-  };
-
-  if(KeyY > HEIGHT) {
-    KeyY = HEIGHT;
-  };
-
-  oscillator.frequency.value = (KeyX/WIDTH) * maxFreq;
-  gainNode.gain.value = (KeyY/HEIGHT) * maxVol;
-
-  canvasDraw();
-}
-
-function switchChannel() {
-
-}
