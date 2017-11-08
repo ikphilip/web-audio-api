0b6439d2e3901237bc5274a1fd88bc5fbff2665d
Philip Curley <philip@interactiveknowledge.com>
2017-11-08 11:48:39 -0500

    Create stereo to mono example

--- scripts/app.js
+++ scripts/app.js
@@ -1,48 +1,73 @@
+function $ (selector, el) {
+  if (!el) {el = document;}
+  return el.querySelector(selector);
+}
+function $$ (selector, el) {
+  if (!el) {el = document;}
+  return el.querySelectorAll(selector);
+  // Note: the returned object is a NodeList.
+  // If you'd like to convert it to a Array for convenience, use this instead:
+  // return Array.prototype.slice.call(el.querySelectorAll(selector));
+}
+
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 // Collect the distinct audio sources
-var frontLeft = document.querySelector('.front-left');
-var frontRight = document.querySelector('.front-right');
-var surroundLeft = document.querySelector('.surround-left');
-var surroundRight = document.querySelector('.surround-right');
+var stereoSource = document.querySelector('.stereo-source');
 
 // Add controls and fix Chrome CORS restriction
-frontLeft.controls = true;
-frontLeft.src = 'media/aintnoman-mono.aac';
-frontRight.controls = true;
-frontRight.src = 'media/sitting-on-top.mp3';
-surroundLeft.controls = true;
-surroundLeft.src = 'media/sitting-on-top.mp3';
-surroundRight.controls = true;
-surroundRight.src = 'media/aintnoman-mono.aac';
+stereoSource.controls = true;
+stereoSource.src = 'media/left-right-stereo-voice.mp3';
 
 // createMediaElementSource grabs a resource referenced in DOM
-var sourceFL = audioCtx.createMediaElementSource(frontLeft);
-var sourceFR = audioCtx.createMediaElementSource(frontRight);
-var sourceSL = audioCtx.createMediaElementSource(surroundLeft);
-var sourceSR = audioCtx.createMediaElementSource(surroundRight);
+var source = audioCtx.createMediaElementSource(stereoSource);
+
+// Splitter node
+var splitter = audioCtx.createChannelSplitter();
+source.connect(splitter);
 
 // gainNode only has a single I/O so each source must have it's own
-var gainNodeFL = audioCtx.createGain();
-sourceFL.connect(gainNodeFL);
-
-var gainNodeFR = audioCtx.createGain();
-sourceFR.connect(gainNodeFR);
-
-var gainNodeSL = audioCtx.createGain();
-sourceSL.connect(gainNodeSL);
-
-var gainNodeSR = audioCtx.createGain();
-sourceSR.connect(gainNodeSR);
+var gainNode = audioCtx.createGain();
+splitter.connect(gainNode);
 
 // Create an output stream which merges 4 channels
-var merger = audioCtx.createChannelMerger(4);
+var merger = audioCtx.createChannelMerger(2);
 
 // Now merge the streams into individual channels
-gainNodeFL.connect(merger, 0, 0);
-gainNodeFR.connect(merger, 0, 1);
-gainNodeSL.connect(merger, 0, 2);
-gainNodeSR.connect(merger, 0, 3);
+splitter.connect(merger, 0, 1);
+splitter.connect(merger, 1, 1);
 
 // Connect to output
 merger.connect(audioCtx.destination);
+
+// Handle switching channels in current output
+var switchChannels = function(element) {
+  source.disconnect();
+  splitter.disconnect();
+  merger.disconnect();
+  source.connect(splitter);
+  
+  switch (element.id) {
+    case 'stereo':
+      splitter.connect(merger, 0, 0);
+      splitter.connect(merger, 1, 1);
+      break;
+    case 'left':
+      splitter.connect(merger, 0, 0);
+      splitter.connect(merger, 1, 0);
+      break;
+    case 'right':
+      splitter.connect(merger, 0, 1);
+      splitter.connect(merger, 1, 1);
+      break;
+  }
+
+  merger.connect(audioCtx.destination);
+};
+
+// Create event listener for radio selection
+$$('input').forEach(function( element ) {
+  element.addEventListener('click', function (event) {
+    switchChannels(event.target);
+  });
+});
